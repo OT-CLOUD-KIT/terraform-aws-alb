4f8529b24ee3c58d362eeef0e43dfc386f68258c
Rishabh Sharma <rishabh.sharma@opstree.com>
2022-02-24 19:25:38 +0530

    Added arn output for every resource

--- output.tf
+++ output.tf
@@ -2,3 +2,18 @@ output "dns_name" {
   description = "DNS of ALB"
   value = aws_lb.alb.dns_name
 }
+
+output "aws_lb_arn" {
+  description = "ARN of alb"
+  value = aws_lb.alb.arn
+}
+
+output "alb_listener_arn" {
+  description = "ARN of alb listener"
+  value = aws_alb_listener.alb_listener.arn
+}
+
+output "alb_listener1_arn" {
+  description = "ARN of alb listener-1"
+  value = aws_alb_listener.alb_listener1.arn
+}
